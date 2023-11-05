de289ff671db03d05030943176bee6a616a8a42f
Chepeingener <2202032@utrivieramaya.edu.mx>
2023-11-04 20:35:05 -0600

    first commit

---
+++ Introduction/ScripIntroduction2.js
@@ -0,0 +1,9 @@
+// Los comentarios pueden hacer que el código sea legible.
+// Este es mi primer código JS.
+// Los comentarios pueden hacer que el código sea legible,
+// fácil de reutilizar e informativo
+
+let name = 'Chepe'
+let student = true
+let age = undefined
+let address = null
---
+++ Introduction/ScriptIntroducion.js
@@ -0,0 +1,45 @@
+// Para ejecutar cada ejercicio, elimina "/*" y "*/" para poder revisar el ejercicio. 
+
+/*
+console.log(typeof 'César')
+console.log(typeof 18)
+console.log(typeof null)
+console.log(typeof undefined)
+*/
+
+// Declarar 4 variables sin asignar valores.
+// Área del trapecio
+/*
+let BaseMayor
+let BaseMenor
+let altura
+let resul
+*/
+
+// Declarar variables asignando valores.
+// Datos personales
+/*
+let name = 'America'
+let lastName = 'Chavez'
+let age = '18'
+let weight = '80'
+*/
+
+// Declarar variables asignando mi nombre.
+/*
+let nombre = 'Jose'
+let apellido = 'De la Cruz'
+let EstadoCivil = 'Soltero'
+let país = 'México'
+let edad = 19
+*/
+
+// Declarar las variables anteriores en una sola línea.
+// let nombre = 'Jose', apellido = 'De la Cruz', EstadoCivil = 'Soltero', país = 'México', edad = 19
+// Declara variables "myAge" y "your Age" y asignar valores iniciales.
+
+let myAge = 19
+let yourAge = 18
+
+console.log('Mi edad es: '+ myAge)
+console.log('Tu edad es: '+yourAge)
